730b603331dafb3de522019e8527bc51a40cafec
Ivan Shcherbanyuk <ishcherbanyuk@gmail.com>
2016-07-24 15:28:56 +0300

    Updated Illustrators Detail View

--- SRC/TopUkrainianIllustrators/Controllers/IllustratorsController.cs
+++ SRC/TopUkrainianIllustrators/Controllers/IllustratorsController.cs
@@ -10,6 +10,12 @@ namespace TopUkrainianIllustrators.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.Name = "Volodymyr Holozubov";
+            ViewBag.From = "Kyiv";
+            ViewBag.AlmaMater = "Taras Shevchenko State Art School";
+            ViewBag.MagnumOpus = "<p><strong>Maggie Magpie</strong> (1970, Kyiv, Veselka), <strong>Two chanticleers</strong> (1970, Kyiv, Veselka)</p>";
+
             return View();
         }
     }
