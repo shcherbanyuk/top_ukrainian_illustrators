7e10c58ae0b9e84de3549125c389cb84985d9b17
Ivan Shcherbanyuk <ishcherbanyuk@gmail.com>
2016-07-25 20:12:50 +0300

    Added Illustrators model

---
+++ SRC/TopUkrainianIllustrators/Models/Illustrators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopUkrainianIllustrators.Models
+{
+    public class Illustrators
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string From { get; set; }
+        public string AlmaMater { get; set; }
+        public string MagnumOpusHtml { get; set; }
+    }
+}
